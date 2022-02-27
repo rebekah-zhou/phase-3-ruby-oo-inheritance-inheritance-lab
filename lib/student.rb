739ad57e90f71db39a96557672ce3edfbe6b6e98
Rebekah Zhou <rebekahzhou@gmail.com>
2022-02-27 14:12:09 -0800

    commit tree d70d40e2abd8cb2219b9314a5ca1d7bd3d59b6ee

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require_relative './user'
 
+class Student < User
+    attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledgeStr)
+        @knowledge << knowledgeStr
+    end
+
+    
 end
